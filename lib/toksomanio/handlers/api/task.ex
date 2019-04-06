123f4e6d11a319001905fc096efc4493a5ce0e27
Gabriel Malaquias <gabriel07malakias@gmail.com>
2019-04-06 11:13:57 +0200

    Sort feature done!

--- lib/toksomanio/handlers/api/task.ex
+++ lib/toksomanio/handlers/api/task.ex
@@ -1,48 +1,60 @@
 defmodule Toksomanio.Handlers.Api.Task do
-  require IEx
-  def call(_params, ["text/plain"]) do
-    {200, "test"}
+
+  alias Toksomanio.Handlers.Api.Task.TSort, as: TopologicalSort
+  alias Toksomanio.Handlers.Api.Task.Sort, as: Sort
+  alias Toksomanio.Presenters.Task.Json, as: JsonPresenter
+  alias Toksomanio.Presenters.Task.Text, as: TextPresenter
+
+  def call(params, ["text/plain"]) do
+    result = params["tasks"]
+    |> tsort()
+    |> sort(params["tasks"])
+    |> to_text()
   end
 
   def call(params, ["application/json"]) do
-    sort(params["tasks"])
-
-    {200, "test"}
+    result = params["tasks"]
+    |> tsort()
+    |> sort(params["tasks"])
+    |> to_json
   end
 
   def call(params, _unknown) do
     {406, "Not acceptable"}
   end
 
-  defp non_dependent(commands) do
-    Enum.filter(commands, fn c ->
-      c["requires"] == nil
-    end)
+
+  defp to_text(:error) do
+    body = TextPresenter.error
+
+    {422, body}
   end
 
-  defp dependent(commands) do
-    Enum.filter(commands, fn c ->
-      c["requires"] != nil
-    end)
+  defp to_text(tasks) do
+    body = TextPresenter.call(tasks)
+
+    {200, body}
   end
 
-  def sort(commands) do
-    dependent_commands = dependent(commands)
-    non_dependent_commands = non_dependent(commands)
+  defp to_json(:error) do
+    body = JsonPresenter.error
 
-    IO.inspect(dependent_commands)
-    IO.inspect(non_dependent_commands)
-
-    sort(dependent_commands, non_dependent_commands)
+    {422, body}
   end
 
-  def sort(remaining, executed) do
-    [first | tail] = remaining
+  defp to_json(tasks) do
+    body = JsonPresenter.call(tasks)
 
-    sort(tail, [first | executed])
+    {200, body}
   end
 
-  def sort([], executed) do
-    executed
+  defp tsort(tasks) do
+    TopologicalSort.apply(tasks)
+  end
+
+  defp sort({:error, tasks}, input), do: :error
+
+  defp sort({:ok, tasks}, input) do
+    Sort.apply(tasks, input)
   end
 end
